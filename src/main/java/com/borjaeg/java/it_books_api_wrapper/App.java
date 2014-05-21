e54202d50a190a724ec482e8a8e4eff1be34070e
Borja A. Espejo-Garcia <borja3790@gmail.com>
2014-05-21 17:23:04 +0200

    first commit

---
+++ src/main/java/com/borjaeg/java/it_books_api_wrapper/App.java
@@ -0,0 +1,13 @@
+package com.borjaeg.java.it_books_api_wrapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
